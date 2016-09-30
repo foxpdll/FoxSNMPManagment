ac16650799e3f4f3a38210b3dc00281a5d85b241
FoxPDLL <foxpdll@mail.ru>
2016-09-30 17:46:02 +0300

    Update foxsnmpman.go

--- foxsnmpman.go
+++ foxsnmpman.go
@@ -5,8 +5,9 @@ import (
     "log"
     "net/http"
     "encoding/base64"
+    "time"
     "flag"
-    "database/sql"
+    _ "database/sql"
     _ "github.com/go-sql-driver/mysql"
     "github.com/alouca/gosnmp"
     gosnmp2 "github.com/soniah/gosnmp"
@@ -16,8 +17,6 @@ import (
 
 var (
     addr = flag.String("addr", ":81", "http service address")
-
-
     ifType = []string{"other", "regular1822", "hdh1822", "ddnX25", "rfc877x25", "ethernetCsmacd", "iso88023Csmacd", "iso88024TokenBus", "iso88025TokenRing", "iso88026Man", "starLan", "proteon10Mbit", "proteon80Mbit", "hyperchannel", "fddi", "lapb", "sdlc", "ds1", "e1", "basicISDN", "primaryISDN", "propPointToPointSerial", "ppp", "softwareLoopback", "eon", "ethernet3Mbit", "nsip", "slip", "ultra", "ds3", "sip", "frameRelay", "rs232", "para", "arcnet", "arcnetPlus", "atm", "miox25", "sonet", "x25ple", "iso88022llc", "localTalk", "smdsDxi", "frameRelayService", "v35", "hssi", "hippi", "modem", "aal5", "sonetPath", "sonetVT", "smdsIcip", "propVirtual", "propMultiplexor", "ieee80212", "fibreChannel", "hippiInterface", "frameRelayInterconnect", "aflane8023", "aflane8025", "cctEmul", "fastEther", "isdn", "v11", "v36", "g703at64k", "g703at2mb", "qllc", "fastEtherFX", "channel", "ieee80211", "ibm370parChan", "escon", "dlsw", "isdns", "isdnu", "lapd", "ipSwitch", "rsrb", "atmLogical", "ds0", "ds0Bundle", "bsc", "async", "cnr", "iso88025Dtr", "eplrs", "arap", "propCnls", "hostPad", "termPad", "frameRelayMPI", "x213", "adsl", "radsl", "sdsl", "vdsl", "iso88025CRFPInt", "myrinet", "voiceEM", "voiceFXO", "voiceFXS", "voiceEncap", "voiceOverIp", "atmDxi", "atmFuni", "atmIma", "pppMultilinkBundle", "ipOverCdlc", "ipOverClaw", "stackToStack", "virtualIpAddress", "mpc", "ipOverAtm", "iso88025Fiber", "tdlc", "gigabitEthernet", "hdlc", "lapf", "v37", "x25mlp", "x25huntGroup", "trasnpHdlc", "interleave", "fast", "ip", "docsCableMaclayer", "docsCableDownstream", "docsCableUpstream", "a12MppSwitch", "tunnel", "coffee", "ces", "atmSubInterface", "l2vlan", "l3ipvlan", "l3ipxvlan", "digitalPowerline", "mediaMailOverIp", "dtm", "dcn", "ipForward", "msdsl", "ieee1394", "if-gsn", "dvbRccMacLayer", "dvbRccDownstream", "dvbRccUpstream", "atmVirtual", "mplsTunnel", "srp", "voiceOverAtm", "voiceOverFrameRelay", "idsl", "compositeLink", "ss7SigLink", "propWirelessP2P", "frForward", "rfc1483", "usb", "ieee8023adLag", "bgppolicyaccounting", "frf16MfrBundle", "h323Gatekeeper", "h323Proxy", "mpls", "mfSigLink", "hdsl2", "shdsl", "ds1FDL", "pos", "dvbAsiIn", "dvbAsiOut", "plc", "nfas", "tr008", "gr303RDT", "gr303IDT", "isup", "propDocsWirelessMaclayer", "propDocsWirelessDownstream", "propDocsWirelessUpstream", "hiperlan2", "propBWAp2Mp", "sonetOverheadChannel", "digitalWrapperOverheadChannel", "aal2", "radioMAC", "atmRadio", "imt", "mvl", "reachDSL", "frDlciEndPt", "atmVciEndPt", "opticalChannel", "opticalTransport", "propAtm", "voiceOverCable", "infiniband", "teLink", "q2931", "virtualTg", "sipTg", "sipSig", "docsCableUpstreamChannel", "econet", "pon155", "pon622", "bridge", "linegroup", "voiceEMFGD", "voiceFGDEANA", "voiceDID", "mpegTransport", "sixToFour", "gtp", "pdnEtherLoop1", "pdnEtherLoop2", "opticalChannelGroup", "homepna", "gfp", "ciscoISLvlan", "actelisMetaLOOP", "fcipLink", "rpr", "qam", "lmp", "cblVectaStar", "docsCableMCmtsDownstream", "adsl2", "macSecControlledIF", "macSecUncontrolledIF", "aviciOpticalEther", "atmbond"}
     ifStatus = []string{"up","down","testing","unknown","dormant","notPresent","lowerLayerDown"}
     cdStatus = []string{"ok","open","short","open-short","crosstalk","unknown","count","no-cable","other"}
@@ -30,13 +29,23 @@ func checkIfaces(w http.ResponseWriter, r *http.Request){
     for i := range snmp_coms_r{
         s, err := gosnmp.NewGoSNMP(ip, snmp_coms_r[i], gosnmp.Version2c, 1)
         checkErr(err)
-        resp, err := s.Get(".1.3.6.1.2.1.17.1.1.0")
+        resp,err := s.Get(".1.3.6.1.2.1.17.1.1.0")
         if err == nil{
             isp = false
-            umac := fmt.Sprintf("%X",resp.Variables[0].Value)
+            _ = resp
+//          umac := fmt.Sprintf("%X",resp.Variables[0].Value)
             fmt.Fprintf(w,"<script>function changeAlias(e,num,txt){var code = (e.keyCode ? e.keyCode : e.which); if (code == 13) {window.open('/swSetAlias/%s/'+num+'/'+txt.value);}};</script>",ip)
+
+            fmt.Fprintf(w,"<table border=1 width='100%'><tr><td width='50%' height=300>")
+            fmt.Fprintf(w,"<div style='width:100%%; height:100%%;'><object style='width:100%%; height:100%%;' type='text/html' data='/swMainData/%s'></object></div>",ip)
+            fmt.Fprintf(w,"</td><td width='50%' height=300>")
+            fmt.Fprintf(w,"<div style='width:100%%; height:100%%;'><object style='width:100%%; height:100%%;' type='text/html' data='/ospfIpNeib/%s'></object></div>",ip)
+            fmt.Fprintf(w,"</td></tr><tr><td>")
+            fmt.Fprintf(w,"<div style='width:100%%; height:100%%;'><object style='width:100%%; height:100%%;' type='text/html' data='/swArpTable/%s'></object></div>",ip)
+            fmt.Fprintf(w,"</td></tr></table>")
+
             fmt.Fprintf(w,"<table border=1 width='100%'>")
-            fmt.Fprintf(w,"<tr><th>SwitchMAC</th><th>ifId</th><th>ifAdminStatus</th><th>ifOperStatus</th><th>ifName</th><th>ifAlias</th><th>ifSpeed</th><th>ifType</th><th>ifMAC</th><th>ifInErr</th><th>Mcast</th><th>LLDP&FDB Table</th></tr>")
+            fmt.Fprintf(w,"<tr><th>ifId</th><th>ifAdminStatus</th><th>ifOperStatus</th><th>ifName</th><th>ifAlias</th><th>ifSpeed</th><th>ifType</th><th>ifMAC</th><th>ifInErr</th><th>Mcast</th><th>LLDP&FDB Table</th></tr>")
             ifadmstat, err := s.Walk(".1.3.6.1.2.1.2.2.1.7")
             checkErr(err)
             ifoperstat, err := s.Walk(".1.3.6.1.2.1.2.2.1.8")
@@ -57,6 +66,8 @@ func checkIfaces(w http.ResponseWriter, r *http.Request){
 //          checkErr(err)
             mcast15, _ := s.Walk(".1.3.6.1.4.1.171.12.73.2.1.2.1.4")
             checkErr(err)
+            mcast16, _ := s.Walk(".1.3.6.1.4.1.171.11.113.1.5.2.7.13.1.4")
+            checkErr(err)
             lldpchasid, _ := s.Walk(".1.0.8802.1.1.2.1.4.1.1.5")
             checkErr(err)
             lldpporttype, _ := s.Walk(".1.0.8802.1.1.2.1.4.1.1.6")
@@ -67,14 +78,16 @@ func checkIfaces(w http.ResponseWriter, r *http.Request){
             checkErr(err)
             lldpsysname, _ := s.Walk(".1.0.8802.1.1.2.1.4.1.1.9")
             checkErr(err)
+            lldppeerip, _ := s.Walk(".1.0.8802.1.1.2.1.4.2.1.3")
+            checkErr(err)
             arptable, _ := s.Walk(".1.3.6.1.2.1.4.22.1.2")
             checkErr(err)
             fdbtable, _ := s.Walk(".1.3.6.1.2.1.17.7.1.2.2.1.2")
             checkErr(err)
-            db, err := sql.Open("mysql", mysql_user+":"+mysql_password+"@tcp("+mysql_host+":"+mysql_port+")/"+mysql_db+"?charset=utf8")
-            checkErr(err)
-            stmt, err := db.Prepare("update sw_if set if_admin_status=?, if_oper_status=? where umac=CONV(?,16,10) and if_id=?")
-            checkErr(err)
+////        db, err := sql.Open("mysql", mysql_user+":"+mysql_password+"@tcp("+mysql_host+":"+mysql_port+")/"+mysql_db+"?charset=utf8")
+////        checkErr(err)
+////        stmt, err := db.Prepare("update sw_if set if_admin_status=?, if_oper_status=? where umac=CONV(?,16,10) and if_id=?")
+////        checkErr(err)
             color:="green"
             for i := range ifadmstat {
                 ifadmstat[i].Name=strings.Replace(ifadmstat[i].Name, ".1.3.6.1.2.1.2.2.1.7.", "", 1)
@@ -84,11 +97,17 @@ func checkIfaces(w http.ResponseWriter, r *http.Request){
                 }else {
                     color="#55aa55"
                 }
+                if ifType[iftype[i].Value.(int)-1]=="ethernetCsmacd" && ifspeed[i].Value.(int)/1000000 == 10 {
+                    color="#55aaaa"
+                }
+                if ifStatus[ifadmstat[i].Value.(int)-1]!="up" {
+                    color="#ff3333"
+                }
                 fmt.Fprintf(w,"<tr bgcolor=%s>",color)
-                fmt.Fprintf(w,"<td>%s</td>",umac)
+//              fmt.Fprintf(w,"<td>%s</td>",umac)
                 fmt.Fprintf(w,"<td>%s</td>",ifadmstat[i].Name)
                 fmt.Fprintf(w,"<td>%s<br>",ifStatus[ifadmstat[i].Value.(int)-1])
-                fmt.Fprintf(w,"<a href='#' style='color: yellow;' onclick='window.open(\"/swPortUp/%s/%s\");'>Вкл</a>",ip,ifadmstat[i].Name)
+                fmt.Fprintf(w,"<a href='#' style='color: yellow;' onclick='window.open(\"/swPortUp/%s/%s\");'>Вкл </a>",ip,ifadmstat[i].Name)
                 fmt.Fprintf(w,"<a href='#' style='color: yellow;' onclick='window.open(\"/swPortDown/%s/%s\");'>Выкл</a>",ip,ifadmstat[i].Name)
                 fmt.Fprintf(w,"</td>")
 //              fmt.Fprintf(w,"<td>%s</td>",ifStatus[ifoperstat[i].Value.(int)-1])
@@ -109,6 +128,19 @@ func checkIfaces(w http.ResponseWriter, r *http.Request){
 //              }
 //              fmt.Fprintf(w,"</td>")
                 fmt.Fprintf(w,"<td>")
+
+                for j := range mcast16 {
+
+                    cmip:=strings.Split(mcast16[j].Name,".")[18]
+                    cmip=cmip+"."+strings.Split(mcast16[j].Name,".")[19]
+                    cmip=cmip+"."+strings.Split(mcast16[j].Name,".")[20]
+                    cmip=cmip+"."+strings.Split(mcast16[j].Name,".")[21]
+                    tport:=strings.Split(mcast16[j].Name,".")[22]
+                    if fmt.Sprintf("%d",i+1)==tport {
+                        fmt.Fprintf(w,"%s(%s)<br>",cmip,mcast16[j].Value)
+                    }
+                }
+
                 for j := range mcast15 {
                     b0:=fmt.Sprintf("%X",mcast15[j].Value)[0:8]
                     b1:=fmt.Sprintf("%X",mcast15[j].Value)[8:16]
@@ -138,7 +170,15 @@ func checkIfaces(w http.ResponseWriter, r *http.Request){
                             fmt.Fprintf(w,"<tr><td>PortID</td><td>%s</td></tr>",lldpport[j].Value)
                         }
                         fmt.Fprintf(w,"<tr><td>PortDescr</td><td>%s</td></tr>",lldpportdescr[j].Value)
-                        fmt.Fprintf(w,"<tr><td>SystemName</td><td>%s</td></tr></table>",lldpsysname[j].Value)
+                        fmt.Fprintf(w,"<tr><td>SystemName</td><td>%s</td></tr>",lldpsysname[j].Value)
+//                      fmt.Fprintf(w,"%i",lldppeerip[0].Name)
+                        for k := range lldppeerip {
+                                if strings.Split(lldppeerip[k].Name,".")[13] == fmt.Sprintf("%d",i+1){
+                                lldppeeripc:=strings.Split(lldppeerip[k].Name,".")[17]+"."+strings.Split(lldppeerip[k].Name,".")[18]+"."+strings.Split(lldppeerip[k].Name,".")[19]+"."+strings.Split(lldppeerip[k].Name,".")[20]
+                                fmt.Fprintf(w,"<tr><td>PeerIP</td><td><a href=%s>%s</a></td></tr>",lldppeeripc,lldppeeripc)
+                            }
+                        }
+                        fmt.Fprintf(w,"</table>")
                         for k := range arptable {
                             ipr:=strings.Replace(arptable[k].Name,".1.3.6.1.2.1.4.22.1.2.","",1)
                             ipr=strings.Replace(ipr,strings.Split(ipr,".")[0]+".","",1)
@@ -165,18 +205,11 @@ func checkIfaces(w http.ResponseWriter, r *http.Request){
                 }
                 fmt.Fprintf(w,"</td>")
                 fmt.Fprintf(w,"</tr>\n")
-                _, err = stmt.Exec(ifadmstat[i].Value,ifoperstat[i].Value,umac, ifadmstat[i].Name)
-                checkErr(err)
+////            _, err = stmt.Exec(ifadmstat[i].Value,ifoperstat[i].Value,umac, ifadmstat[i].Name)
+////            checkErr(err)
             }
             fmt.Fprintf(w,"</table><br>")
-
-            fmt.Fprintf(w,"<table border=1 width='100%'><tr><td width='50%' height=300>")
-            fmt.Fprintf(w,"<div style='width:100%%; height:100%%;'><object style='width:100%%; height:100%%;' type='text/html' data='/swMainData/%s'></object></div>",ip)
-            fmt.Fprintf(w,"</td><td width='50%' height=300>")
-            fmt.Fprintf(w,"<div style='width:100%%; height:100%%;'><object style='width:100%%; height:100%%;' type='text/html' data='/ospfIpNeib/%s'></object></div>",ip)
-            fmt.Fprintf(w,"</td></tr><tr><td>")
-            fmt.Fprintf(w,"<div style='width:100%%; height:100%%;'><object style='width:100%%; height:100%%;' type='text/html' data='/swArpTable/%s'></object></div>",ip)
-            fmt.Fprintf(w,"</td></tr></table></body></html>")
+            fmt.Fprintf(w,"</body></html>")
             return
         }
     }
@@ -194,7 +227,7 @@ func setSnmpStr(ip string, oid string, val string){
         }
         defer gosnmp2.Default.Conn.Close()
         var pdus []gosnmp2.SnmpPDU
-        pdus = append(pdus, gosnmp2.SnmpPDU{oid, 4, val})
+        pdus = append(pdus, gosnmp2.SnmpPDU{oid, 4, val, nil})
         gosnmp2.Default.Set(pdus)
         gosnmp2.Default.Conn.Close()
     }
@@ -210,7 +243,7 @@ func setSnmpInt(ip string, oid string, val int){
         }
         defer gosnmp2.Default.Conn.Close()
         var pdus []gosnmp2.SnmpPDU
-        pdus = append(pdus, gosnmp2.SnmpPDU{oid, 2, val})
+        pdus = append(pdus, gosnmp2.SnmpPDU{oid, 2, val, nil})
         gosnmp2.Default.Set(pdus)
         gosnmp2.Default.Conn.Close()
     }
@@ -278,6 +311,9 @@ func swPortDown(w http.ResponseWriter, r *http.Request) {
     setSnmpInt(ip,port,2)
 }
 
+//func swFdbTable(w http.ResponseWriter, r *http.Request) {
+//}
+
 func swArpTable(w http.ResponseWriter, r *http.Request) {
     ip:=r.URL.Path[12:]
     for i := range snmp_coms_r{
@@ -323,6 +359,11 @@ func swMainData(w http.ResponseWriter, r *http.Request) {
             fmt.Fprintf(w,"<tr><td>System Name</td><td>%s</td></tr>\n",resp.Variables[0].Value.(string))
             resp, _ = s.Get(".1.3.6.1.2.1.1.6.0")
             fmt.Fprintf(w,"<tr><td>System Location</td><td>%s</td></tr>\n",resp.Variables[0].Value.(string))
+            resp, _ = s.Get(".1.3.6.1.4.1.171.12.15.2.3.1.21.0")
+
+            stpt := time.Unix(int64(resp.Variables[0].Value.(int)/100),0)
+            fmt.Fprintf(w,"<tr><td>STP Last Change</td><td>%d %d:%d:%d</td></tr>\n",stpt.Day()-1,stpt.Hour(),stpt.Minute(),stpt.Second())
+
             fmt.Fprintf(w,"</table>")
             return
         }
@@ -430,15 +471,15 @@ func checkErr(err error) {
     }
 }
 
-var mysql_host="127.0.0.1"
-var mysql_port="3306"
-var mysql_user="user"
-var mysql_password="password"
-var mysql_db="foxnms"
+////var mysql_host="217.107.196.60"
+////var mysql_port="3306"
+////var mysql_user="foxpdll"
+////var mysql_password="qwe123"
+////var mysql_db="foxnms"
 var snmp_coms_r=[...]string{"public", "readzt"}
 var snmp_coms_w=[...]string{"private", "wrzt"}
-var http_user="user"
-var http_password="password"
+var http_user="tech"
+var http_password="tech123"
 
 
 
